7e771f58cc32a4f41e835b9d3231b60033c441be
dmxbasil <77526924+dmxbasil@users.noreply.github.com>
2021-07-13 23:42:22 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -47,13 +47,4 @@ Iam created by
 @basildmx
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/basildmx">Basil DMX</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/dmxbasil/adv-autofilter-v3">Click Me</a></i>
-"""
